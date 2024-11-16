bf251e2aaf5b15ae4e2536b416ae6793090d97d5
Daniel Hansen <dchansen06@gmail.com>
2024-11-15 17:57:08 -0800

    Flash for correct message

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -113,9 +113,6 @@ int main(void)
       case HAL_OK:
         // Communication is completed
         // Compare the sent and received buffers
-        uint8_t recieved[BUFFERSIZE];
-        memcpy(recieved, aRxBuffer, BUFFERSIZE);
-
         if (!Buffercmp((uint8_t *)aTxBuffer, (uint8_t *)aRxBuffer, BUFFERSIZE))
         {
           Error_Handler();  // Transfer error :(
@@ -123,7 +120,7 @@ int main(void)
 
         // Do something with the data we got! Probably configure aRxBuffer
         memcpy(aTxBuffer, "correct", BUFFERSIZE);
-        if (memcmp(recieved, "sending", BUFFERSIZE)) {
+        if (!memcmp(aRxBuffer, "sending", BUFFERSIZE)) {
           for(int i = 0; i < 6; i++) {
             HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_5);
             HAL_Delay(500);
