a901c8b1053552e5eeb75fe28d9a43b9ab02d598
Daniel Hansen <dchansen06@gmail.com>
2024-11-15 17:57:45 -0800

    Conf new msg

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -119,13 +119,15 @@ int main(void)
         }
 
         // Do something with the data we got! Probably configure aRxBuffer
-        memcpy(aTxBuffer, "correct", BUFFERSIZE);
         if (!memcmp(aRxBuffer, "sending", BUFFERSIZE)) {
           for(int i = 0; i < 6; i++) {
             HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_5);
             HAL_Delay(500);
           }
         }
+
+        /* New message to send */
+        memcpy(aTxBuffer, "correct", BUFFERSIZE);
 
         break;
 
