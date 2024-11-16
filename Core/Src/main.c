a4530af59eb1d1b6d02f52ea4b312e066171580c
Daniel Hansen <dchansen06@gmail.com>
2024-11-15 17:09:03 -0800

    Adjust msg

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -122,7 +122,7 @@ int main(void)
 
         // Do something with the data we got! Probably configure aRxBuffer
         strncpy(aTxBuffer, "correct", BUFFERSIZE);
-        if (strcmp(aRxBuffer, "1stsend")) {
+        if (strcmp(aRxBuffer, "sending")) {
           while(1) {
             HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_5);
             HAL_Delay(50);
