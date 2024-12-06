357cfdf59dbbb88dc32a25245025b8dac05f7403
Daniel Hansen <dchansen06@gmail.com>
2024-12-05 21:09:41 -0800

    Typos

--- StreamBuffer/testingStreamBuffer.c
+++ StreamBuffer/testingStreamBuffer.c
@@ -3,31 +3,31 @@
 int main()
 {
         printf("Prepare to be amazed:\n");
-    addToBuffer("testingthisout!", 16, 'r');
-    printBuffer();
- 
-    updateHardwareBuffer('r');
-    printBuffer();
+        addToBuffer("testingthisout!", 16, 'r');
+        printBuffer();
 
-    addToBuffer("idonotknowhowtospellanything", 28, 't');
-    printBuffer();
-    
-    updateHardwareBuffer('t');
-    printBuffer();
+        updateHardwareBuffer('r');
+        printBuffer();
 
-    resetSoftwareBuffer('r');
-    printBuffer();
+        addToBuffer("idonotknowhowtospellanything", 28, 't');
+        printBuffer();
 
-    updateHardwareBuffer('t');
-    printBuffer();
+        updateHardwareBuffer('t');
+        printBuffer();
 
-    updateHardwareBuffer('t');
-    printBuffer();
+        resetSoftwareBuffer('r');
+        printBuffer();
 
-    updateHardwareBuffer('t');
-    printBuffer();
+        updateHardwareBuffer('t');
+        printBuffer();
 
-    addToBuffer("cheese", 7, 'r');
-    addToBuffer("pizza", 6,'r');
-    printBuffer();
+        updateHardwareBuffer('t');
+        printBuffer();
+
+        updateHardwareBuffer('t');
+        printBuffer();
+
+        addToBuffer("cheese", 7, 'r');
+        addToBuffer("pizza", 6,'r');
+        printBuffer();
 }
