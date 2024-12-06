48c20065b76548bbb6f1d89fe9a1d764dfbbd282
Daniel Hansen <dchansen06@gmail.com>
2024-12-05 19:49:41 -0800

    Remove extra lib

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -26,7 +26,7 @@
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
 #include <string.h>
-#include <stdbool.h>
+
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
